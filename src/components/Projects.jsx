6835ba9e7b9c1100b5091101ff28d5e5efdbe1ce
russom214 <russom214@gmail.com>
2024-01-15 18:32:59 -0500

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -63,7 +63,8 @@ const Projects = () => {
           </div>
 
           <div>
-            <ProjectItem className=""
+            <ProjectItem
+              className=""
               title="OpenStream AI "
               backgroundImg="/../assets/projects/openstream.png"
               projectUrl="/property"
@@ -83,12 +84,13 @@ const Projects = () => {
                 <RiRadioButtonFill className="pr-1" /> Tailwind
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" />OpenAI API
+                <RiRadioButtonFill className="pr-1" />
+                OpenAI API
               </p>
             </div>
-            <p className="m-6">
+            {/* <p className="m-6">
             OpenStream is a project within the LangChain ecosystem that seamlessly integrates various tools powered by OpenAI language models. This multifaceted platform offers a range of functionalities, including real-time streaming capabilities, efficient PDF conversion, and intelligent summarization.
-            </p>
+            </p> */}
             <div className="flex">
               <Link href="https://netflix-reactjs-f2e51.web.app">
                 <button className="px-8 py-2 m-8 mt-4 mr-8">Live Demo</button>
@@ -97,44 +99,43 @@ const Projects = () => {
                 <button className="px-8 py-2 m-8 mt-4 mr-8">Code</button>
               </Link>
             </div>
-
           </div>
-            
-            <div className="m-8 p-8">
-              <h2>Little Lemon Restaurant</h2>
-              <div className="m-4 flex justify-between col-span-2">
-                <p className="text-gray-600 py-2 flex items-center">
-                  <RiRadioButtonFill className="pr-1" /> React
-                </p>
-                <p className="text-gray-600 py-2 flex items-center">
-                  <RiRadioButtonFill className="pr-1" /> Javascript
-                </p>
-                <p className="text-gray-600 py-2 flex items-center">
-                  <RiRadioButtonFill className="pr-1" /> Figma
-                </p>
-                <p className="text-gray-600 py-2 flex items-center">
-                  <RiRadioButtonFill className="pr-1" /> CSS
-                </p>
-                <p className="text-gray-600 py-2 flex items-center">
-                  <RiRadioButtonFill className="pr-1" /> HTML
-                </p>
-              </div>
-              <p className="m-6">
-                This is my first UI/UX project I built it with Figma, React and
-                JS as well. We created a functional booking system for the
-                little lemon restaurant with fully pleged Wireframing and
-                Prototyping concepts in Figma. I have the links for both source
-                code and wireframes below.
+
+          <div className="m-8 p-8">
+            <h2>Little Lemon Restaurant</h2>
+            <div className="m-4 flex justify-between col-span-2">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Javascript
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Figma
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> CSS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> HTML
               </p>
             </div>
-              <div>
-              <ProjectItem
-                title="Little Lemon Restaurant "
-                backgroundImg="/../assets/projects/little-lemon.png"
-                projectUrl="/property"
-                tech="React JS"
-              />
-            </div>
+            <p className="m-6">
+              This is my first UI/UX project I built it with Figma, React and JS
+              as well. We created a functional booking system for the little
+              lemon restaurant with fully pleged Wireframing and Prototyping
+              concepts in Figma. I have the links for both source code and
+              wireframes below.
+            </p>
+          </div>
+          <div>
+            <ProjectItem
+              title="Little Lemon Restaurant "
+              backgroundImg="/../assets/projects/little-lemon.png"
+              projectUrl="/property"
+              tech="React JS"
+            />
+          </div>
         </div>
       </div>
     </div>
